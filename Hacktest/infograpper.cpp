9a9202e281bed73cd5f4ad890835ff3e2b41b926
Chivier <chivier.humber@gmail.com>
2019-08-07 04:47:53 +0800

    email test

--- Hacktest/infograpper.cpp
+++ Hacktest/infograpper.cpp
@@ -4,8 +4,10 @@
 #include <vector>
 #include <cstdio>
 #include <cstdlib>
+#include <string.h>
 #include <algorithm>
 #include <time.h>
+#include <errno.h>
 #include <set>
 #include <unistd.h>
 //#include "quickmail.h"
@@ -18,6 +20,10 @@ using namespace std;
 // *  "products":[     is the final tag
 // *  "style":"        is the color tag
 
+const char From_Mail[] = "huangyeqi@mail.ustc.edu.cn";
+const char To_Mail[] = "chivier.humber@gmail.com";
+const char Subject_New_Update[] = "New Shoes";
+const char Message_New_Update[] = "New Shoes!";
 const string Detail_Tag = "details\":{";
 const string Style_Tag = "\"style\":\"";
 const string Large_Tag = "large";
@@ -213,6 +219,27 @@ void ShopList_Deal(const std::string &str)
     }
 }
 
+int sendmail(const char *to, const char *from, const char *subject, const char *message)
+{
+    int retval = -1;
+    FILE *mailpipe = popen("/usr/lib/sendmail -t", "w");
+    if (mailpipe != NULL)
+    {
+        fprintf(mailpipe, "To: %s\n", to);
+        fprintf(mailpipe, "From: %s\n", from);
+        fprintf(mailpipe, "Subject: %s\n\n", subject);
+        fwrite(message, 1, strlen(message), mailpipe);
+        fwrite(".\n", 1, 2, mailpipe);
+        pclose(mailpipe);
+        retval = 0;
+    }
+    else
+    {
+        perror("Failed to invoke sendmail");
+    }
+    return retval;
+}
+
 void Get_Details()
 {
     system("curl 'https://www.footlocker.com/category/brands/jordan.html' -H 'authority: www.footlocker.com' -H 'pragma: no-cache' -H 'cache-control: no-cache' -H 'upgrade-insecure-requests: 1' -H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36' -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3' -H 'accept-encoding: gzip, deflate, br' -H 'accept-language: en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7' --compressed > rawdata.txt");
@@ -300,4 +327,5 @@ int main()
     freopen("rawdata.txt", "r", stdin);
     freopen("list.txt", "w", stdout);
     Get_Details();
+    sendmail(From_Mail, To_Mail, Subject_New_Update, Message_New_Update);
 }
